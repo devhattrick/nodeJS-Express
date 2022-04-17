04885423151a12434209b3ac382eb2cf0883d261
devhattrick <peerapol.panmuang@gmail.com>
2022-04-17 22:32:37 +0700

    test add route

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 const http = require('http')
 const fs = require('fs') // module file system
+const express = require('express')
+const app = express()
 
-const homePage = fs.readFileSync('home.html')
-const aboutPage = fs.readFileSync('about.html')
-const contactPage = fs.readFileSync('contact.html')
-const notFoundPage = fs.readFileSync('notfound.html')
-
-const server = http.createServer((req,res)=>{
-    if(req.url === '/about'){
-        res.end(aboutPage)
-    }else if(req.url === '/contact'){
-        res.end(contactPage)
-    }else if(req.url === '/'){
-        res.end(homePage)
-    }else{
-        res.writeHead(404)
-        res.end(notFoundPage)
-    }
+app.listen(3000,()=>{
+    console.log('App listening on port 3000')
 })
 
-server.listen(3000)
+app.get('/',(req,res)=>{
+    res.json({
+        name:'hattrick'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.json({
+        name:'About Page'
+    })
+})
+
+// const homePage = fs.readFileSync('home.html')
+// const aboutPage = fs.readFileSync('about.html')
+// const contactPage = fs.readFileSync('contact.html')
+// const notFoundPage = fs.readFileSync('notfound.html')
+
+// const server = http.createServer((req,res)=>{
+//     if(req.url === '/about'){
+//         res.end(aboutPage)
+//     }else if(req.url === '/contact'){
+//         res.end(contactPage)
+//     }else if(req.url === '/'){
+//         res.end(homePage)
+//     }else{
+//         res.writeHead(404)
+//         res.end(notFoundPage)
+//     }
+// })
+
+// server.listen(3000)
